98525e5281af24c9b4c168883896855210c98454
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-05-09 14:59:37 +0530

    directives successfully

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,11 +5,12 @@ import {Component} from '@angular/core'; //decorator in angular to make the clas
     template: 
     `<h2>{{ getTitle() }}</h2>  
     <ul>
-    <li *ngFor="let course of courses">  //let course become items in array courses
+    <li *ngFor="let course of courses">  
     {{ course }}
     </li>
     </ul>
-    `    //exapmle for data binding ,interpolation
+    `    //exapmle for data binding ,interpolation //let course become items in array courses
+
 })
 export class CoursesComponent {
     title= 'List of courses';
